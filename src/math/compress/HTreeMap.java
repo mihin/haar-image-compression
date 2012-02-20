2089763af088afb4b0e388a0ce68a6922262798c
Mikhail Prisheltsev <mprisheltsev@neoline.biz>
2012-02-20 10:28:31 +0400

    added missed file HTree

---
+++ src/math/compress/HTreeMap.java
@@ -0,0 +1,62 @@
+package math.compress;
+
+import java.util.ArrayList;
+
+public class HTreeMap {
+	private ArrayList<HCode> items;
+	public HTreeMap() {
+//		super();
+		items = new ArrayList<HCode>();
+	}
+	public boolean add(int value, String code) {
+		return items.add(new HCode(value, code));
+	}
+	@Override
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		if (items!=null){
+			for (HCode item:items){
+				sb.append(item.toString()+", ");
+			}
+		} else sb.append("null");
+		return sb.toString();
+	}
+	
+	public boolean[] getBits(int value) throws Exception{
+		for (HCode c:items)
+			if (value == c.getValue()) return c.getBits();
+		return null;
+	}
+}
+
+class HCode{
+	private int value;
+	private String code;
+	public HCode(int value, String code) {
+		super();
+		this.value = value;
+		this.code = code;
+	}
+	@Override
+	public String toString() {
+		return value+"->\""+code+"\"";
+	}
+	public int getValue() {
+		return value;
+	}
+	public String getCode() {
+		return code;
+	}
+	public boolean[] getBits() throws Exception {
+		boolean[] bits = new boolean[code.length()];
+//		for (Character c:code.toCharArray()){
+//			if (c.equals('0'))
+//		}
+		for (int i=0;i<code.length();i++)	{
+			if (code.charAt(i)=='0') bits[i] = false; 
+			else if (code.charAt(i)=='1') bits[i] = true;
+			else throw new Exception("blabla");
+		}
+		return bits;
+	}
+}
